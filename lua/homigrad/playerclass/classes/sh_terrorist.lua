abd7f67197a040743450cce0c8a1be044b6eaee0
toilet-master <109538221+toilet-master@users.noreply.github.com>
2026-02-04 21:23:53 +0300

    Update sh_terrorist.lua

--- lua/homigrad/playerclass/classes/sh_terrorist.lua
+++ lua/homigrad/playerclass/classes/sh_terrorist.lua
@@ -4,21 +4,26 @@ function CLASS.Off(self)
     if CLIENT then return end
 end
 
-local models = {
-    "models/player/arctic.mdl",
-    "models/player/phoenix.mdl",
-    "models/player/leet.mdl",
-    "models/player/guerilla.mdl"
+local masks = {
+    "arctic_balaclava",
+    "phoenix_balaclava",
+    "bandana"
 }
 
 function CLASS.On(self)
     if CLIENT then return end
     ApplyAppearance(self,nil,nil,nil,true)
-    local Appearance = self.CurAppearance or hg.Appearance.GetRandomAppearance()
-    Appearance.AAttachments = ""
-    Appearance.AColthes = ""
-    self:SetPlayerColor(Color(165,0,0):ToVector())
-    self:SetModel(models[math.random(#models)])
+    timer.Simple(.1,function()
+        local Appearance = self.CurAppearance or hg.Appearance.GetRandomAppearance()
+
+        Appearance.AAttachments = {
+            masks[math.random(#masks)],
+            "terrorist_band"
+        }
+        self:SetNetVar("Accessories", Appearance.AAttachments or "none")
+        
+        self.CurAppearance = Appearance
+    end)
 end
 
 function CLASS.Guilt(self, victim)
