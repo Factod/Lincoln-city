3475dfede5ee9afe2a08775dc2bdce2997e84b92
sarre <166627315+Factod@users.noreply.github.com>
2026-02-17 20:18:05 +0500

    meow
    mrph

---
+++ lua/homigrad/playerclass/classes/sh_furrymeow.lua
@@ -0,0 +1,62 @@
+-- люблю милых котиков!!!!!!!!!!
+local fur_pain = {
+    "zbattle/furry/exp5.wav", "zbattle/furry/exp6.wav", "zbattle/furry/exp7.wav",
+    "zbattle/furry/exp8.wav", "zbattle/furry/exp9.wav", "zbattle/furry/exp10.wav",
+    "zbattle/furry/exp11.wav", "zbattle/furry/exp12.wav", "zbattle/furry/exp13.wav",
+    "zbattle/furry/exp14.wav", "zbattle/furry/exp15.wav", "zbattle/furry/exp16.wav",
+    "zbattle/furry/exp17.wav", "zbattle/furry/death1.wav", "zbattle/furry/death3.wav",
+    "zbattle/furry/death4.wav", "zbattle/furry/death5.wav",
+}
+
+-- звуков протогенов ЛОГИЧНО нету, я же не тупой еблон
+local uwuspeak_phrases = {
+    "zbattle/furry/cat_mrrp1.ogg", "zbattle/furry/cat_mrrp1.ogg",
+    "zbattle/furry/cat_purr1.ogg", "zbattle/furry/cat_purr2.ogg",
+    "zbattle/furry/mewo.ogg", "zbattle/furry/mrrp.ogg",
+}
+
+local function IsHui(ply)
+    if not IsValid(ply) then return false end
+    -- лучше моделькой мб?
+    return ply:GetModel() == "models/keith3201/ligeia/ligeia_pm.mdl" or ply.PlayerClassName == "meow"
+end
+
+local CLASS = player.RegClass("meow")
+
+function CLASS.Off(self)
+    if CLIENT then return end
+end
+
+function CLASS.On(self)
+    if CLIENT then return end
+    
+    -- рот ипал суко, шляпы и боди группы не убераються :steamsad:
+    ApplyAppearance(self, nil, nil, nil, true)
+    
+    self:SetModel("models/keith3201/ligeia/ligeia_pm.mdl")
+    self:SetBodyGroups("000000000")
+    -- хотябы это норм работает
+    self:SetSubMaterial()
+
+    self.PlayerClassName = "meow"
+end
+
+function CLASS.Guilt(self, Victim)
+    if CLIENT then return end
+    if Victim:GetPlayerClass() == self:GetPlayerClass() then return 1 end
+end
+
+-- ZCITY NIGGASCRIPTHOOK
+hook.Add("HG_ReplacePhrase", "mrphh", function(ent, phrase, muffed, pitch)
+    if IsHui(ent) then
+        local inpain = ent.organism and ent.organism.pain > 60
+        local phr = inpain and table.Random(fur_pain) or table.Random(uwuspeak_phrases)
+        return ent, phr, muffed, pitch
+    end
+end)
+
+hook.Add("HG_ReplaceBurnPhrase", "meowburnn", function(ply, phrase)
+    if IsHui(ply) then
+        return ply, table.Random(fur_pain)
+    end
+end)
